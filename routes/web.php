6eb3904e43b70895a81c3c41003355a57fd91ea6
Eliezer Caliri <lirinhojc@gmail.com>
2023-06-18 22:04:33 -0300

    Ultimas Alterações

--- routes/web.php
+++ routes/web.php
@@ -29,12 +29,16 @@ Route::middleware(['web'])->group(function (){
     Route::get('/', [HomeController::class, 'index'])->name('home.index');
     Route::any('/lista-produtos', [HomeController::class, 'listaProdutos'])->name('home.listaProdutos');
     Route::get('/item/{produto}/{slug}',[HomeController::class, 'details'])->name('home.details');
+    
 });
 
 // Grupo que exige auteticação do usuário
 Route::middleware(['auth'])->group(function (){
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
     Route::put('/admin/edit/{admin}', [AdminController::class, 'edit'])->name('admin.edit');
+    Route::get('/listacliente', [AdminController::class, 'lista'])->name('admin.listaclientes');
+
+ 
 
     Route::get('/produtos', [ProdutosController::class, 'index'])->name('produtos.index');
     Route::get('/produtos/novo', [ProdutosController::class, 'create'])->name('produtos.create');
@@ -43,10 +47,10 @@ Route::middleware(['auth'])->group(function (){
     Route::post('/produtos', [ProdutosController::class, 'store'])->name('produtos.store');
     Route::put('/produtos/{produto}', [ProdutosController::class, 'update'])->name('produtos.update');
     Route::delete('/produtos/{produto}', [ProdutosController::class, 'destroy'])->name('produtos.destroy');
-    Route::get('/listacliente',[UsuarioController::class],'lista')->name('admin.clientes');
 
     Route::get('/categoria', [CategoriaController::class, 'create'])->name('categoria.index');
-        Route::post('/categoria', [CategoriaController::class, 'store'])->name('categoria.store');
+    Route::post('/categoria', [CategoriaController::class, 'store'])->name('categoria.store');
+    Route::delete('/categoria/{id}',[CategoriaController::class,'destroy'])->name('categoria.destroy');
 });
 
 Route::get('/admin/novo', [CadastroAdmController::class, 'index'])->name('cadastroadm.index');
@@ -57,6 +61,7 @@ Route::get('/sair', [AdminController::class, 'logout'])->name('admin.logout');
 Route::post('/entrar', [AdminController::class, 'store'])->name('admin.login.store');
 
 
+
 Route::controller(LoginController::class)->group(function (){
     Route::get('/usuario/novo','index')->name('cadastro.index');
     Route::post('/usuario','store')->name('login.store');
@@ -65,6 +70,9 @@ Route::controller(UsuarioController::class)->group(function (){
     route::get('/login/logar','usuario')->name('usuario.logar');
     route::post('/login/logar','store')->name('usuario.store');
     Route::get('/logout','logout')->name('login.logout');
+    Route::get('/usuario/perfil','perfil')->name('cadastro.cliente');
+    Route::get('/usuario/{usuarios}/edit','edit')->name('cadastro.edit');
+    Route::put('/usuario/{usuarios}','update')->name('usuario.update');
 });
 
 Route::controller(CarrinhoController::class)->group(function (){
